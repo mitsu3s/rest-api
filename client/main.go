3c2b2007045819d5d059a2e52780ffec512e66b8
mitsu3s <mitsuuu.s3s@gmail.com>
2023-11-18 19:24:29 +0900

    Update main.go

--- client/main.go
+++ client/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	// Echo instanceを作成
+	e := echo.New()
+
+	// ルーティングを設定
+	e.GET("/", func(c echo.Context) error {
+		message := map[string]string{
+			"message": "Hello, World!",
+		}
+		return c.JSON(http.StatusOK, message)
+	})
+	// サーバーを開始
+	e.Logger.Fatal(e.Start(":1323"))
 }
